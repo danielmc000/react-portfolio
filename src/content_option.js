01605fb736dd265aa758268b0f3a4e9cb569cee4
danielm000 <danielalberto_7867@hotmail.com>
2022-08-18 19:19:40 -0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "DANIEL";
+const meta = {
+    title: "Daniel Montoya",
+    description: "Soy Daniel Montoya Analista Programador _ Full stack devloper,Actualmente trabajando en Valdivia",
+};
+
+const introdata = {
+    title: "Soy Daniel Montoya",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+import img1 from './assets/images/portfolio3.jpg';
+import img2 from './assets/images/portfolio1.jpg';
+import img3 from './assets/images/portfolio2.jpg';
+import img4 from './assets/images/portfolio4.jpg';
+import img5 from './assets/images/portfolio5.jpg';
+import img6 from './assets/images/portfolio6.jpg';
+
+const dataportfolio = [{
+        img: img1,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://vellavista.netlify.app/",
+    },
+    {
+        img: img2,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/danielmc000/e-commerce",
+    },
+    {
+        img: img3,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://muebleria007.netlify.app/",
+    },
+    {
+        img: img4,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/danielmc000/peliculas",
+    },
+    {
+        img: img5,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://play.google.com/store/apps/details?id=com.dckn.codelab.qr",
+    },
+    {
+        img: img6,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/danielmc000/delirevy_flutter",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
